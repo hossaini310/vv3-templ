65e3a8a07f696968b091ae1c2f51497dec568856
Hossaini Hosain <hossainih03@gmail.com>
2023-05-29 22:13:22 +0200

    🐛 fix(defaultStore.js): change counter variable declaration from let to const The counter variable is now declared as a constant, which improves consistency with the naming conventions as counter is not reassigned.

--- src/stores/defaultStore.js
+++ src/stores/defaultStore.js
@@ -4,7 +4,7 @@ import { ref } from 'vue';
 const useDefaultStore = defineStore('DefaultId', () => {
   const aboutContent = ref('Implementiert von © Hosain Hossaini, HTL Wien West 2023');
   const homeContent = ref('This is a template for a Vue 3 project using the Vite build tool.');
-  let counter = ref(0);
+  const counter = ref(0);
 
   const increaseCounter = () => {
     counter.value += 1;
